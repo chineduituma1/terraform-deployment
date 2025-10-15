a945edae7da1d1f94b873f2ba96046d3d9a1d521
Chinedu Mba <158306503+chineduituma1@users.noreply.github.com>
2025-10-15 23:14:16 +0100

    Update Public_Subnet02.tf

--- Public_Subnet02.tf
+++ Public_Subnet02.tf
@@ -2,9 +2,9 @@ resource "aws_subnet" "terraform_public_subnet02" {
   vpc_id                  = aws_vpc.Terra-vpc.id
   cidr_block              = var.subnet2_cidr
   map_public_ip_on_launch = true
-  availability_zone       = "eu-west-2b"
+  availability_zone       = "eu-west-1b"
 
   tags = {
-    Name = "Terraform-public-subnet02"
+    Name = "Terraform-public-subnet02
   }
 }
